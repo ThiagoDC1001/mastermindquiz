7c7bbc428f9745340095a83726d0e4f9ff3af8ae
ThiagoDC <cmsthiagoazevedo@gmail.com>
2021-01-25 15:43:12 -0300

    código ao final da primeira aula da imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widgets';
+import QuizBackground from "../src/components/QuizBackground"
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%; 
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+              <Widget>
+                <Widget.Header>
+                    <h1>teste teste teste</h1>
+                </Widget.Header>
+                <Widget.Content>
+                Passando raiva nem necessidade
+                </Widget.Content>  
+              </Widget>    
+            <Footer />  
+        </QuizContainer>
+      <GitHubCorner projectUrl="http://https://github.com/ThiagoDC1001/mastermindquiz" />
+    </QuizBackground>
+  )
 }
