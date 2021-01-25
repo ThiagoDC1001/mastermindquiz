4a0da1b6bfe055576ace105291e0b70bd73e4385
ThiagoDC <cmsthiagoazevedo@gmail.com>
2021-01-25 17:53:42 -0300

    adicionando Logo, Background

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Widget from '../src/components/Widgets';
 import QuizBackground from "../src/components/QuizBackground"
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
 export const QuizContainer = styled.div`
   width: 100%; 
@@ -20,16 +21,17 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
         <QuizContainer>
+            <QuizLogo />
               <Widget>
                 <Widget.Header>
-                    <h1>teste teste teste</h1>
+                <h1>Perguntas do Quiz</h1>
                 </Widget.Header>
                 <Widget.Content>
-                Passando raiva nem necessidade
+                Aqui vão as perguntas e as opções
                 </Widget.Content>  
-              </Widget>    
-            <Footer />  
+              </Widget>                
         </QuizContainer>
+            <Footer />  
       <GitHubCorner projectUrl="http://https://github.com/ThiagoDC1001/mastermindquiz" />
     </QuizBackground>
   )
